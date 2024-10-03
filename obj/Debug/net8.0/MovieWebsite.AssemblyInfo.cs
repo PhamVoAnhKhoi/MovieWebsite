3aece4f898c1965e0524f17a6819f43094ce62c3
PhamVoAnhKhoi <anhkhoiphamvo2909@gmail.com>
2024-10-04 05:06:18 +0700

    Big Update Movie ver01

--- obj/Debug/net8.0/MovieWebsite.AssemblyInfo.cs
+++ obj/Debug/net8.0/MovieWebsite.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MovieWebsite")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fd619be8654000aef5b3ac9db9658ca5a809fa9e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f5f75885844da4ef8f4afd57f4ab8ac5053c34b9")]
 [assembly: System.Reflection.AssemblyProductAttribute("MovieWebsite")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MovieWebsite")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
